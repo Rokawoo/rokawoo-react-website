b66a171c36a6fd5c2bd39fd109ee4648cdc9e831
Augustus Sroka <ays36@drexel.edu>
2024-05-07 21:01:55 -0400

    Added NavBar

--- rokawoo-react-website/src/App.tsx
+++ rokawoo-react-website/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from "./App.module.css"
 import Background from './Background';
+import { Navbar } from './components/Navbar/Navbar';
 
 const App: React.FC = () => {
   return (
     <div className={styles.App}>
       <Background />
-
+      <Navbar />
     </div>
   );
 };
