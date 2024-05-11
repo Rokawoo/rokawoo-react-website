72ee22ac4e57bf3196eb951b4ced4ce1897cf8fe
Augustus Sroka <ays36@drexel.edu>
2024-05-11 13:58:15 -0400

    Set Up Wave System to Use Vars From vars.css

--- rokawoo-react-website/src/components/Hero/Hero.tsx
+++ rokawoo-react-website/src/components/Hero/Hero.tsx
@@ -6,28 +6,28 @@ import { getImageUrl } from "../../utils";
 export const Hero = () => {
   return (
     <>
-    <div className={styles.background}>
-    <div className={styles.wave}></div>
+    <div className={styles.backgroundColor}>
+      <div className={styles.wave}></div>
 
-    <section className={styles.container}>
-      <div className={styles.content}>
-        <h1 className={styles.title}>Hello, I'm Rokawoo</h1>
-        <p className={styles.description}>
-          ⚠ CAUTION: This person is super cool!
-        </p>
-        <a href="#contact" className={styles.contactBtn}>
-          Contact Me
-        </a>
-      </div>
-      <img
-        src={getImageUrl("hero/heroImage.png")}
-        alt="rokawoo-mascot-hero"
-        className={styles.heroImg}
-        draggable="false"
-      />
-      <div className={styles.topBlur} />
-      <div className={styles.bottomBlur} />
-    </section>
+      <section className={styles.container}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Hello, I'm Rokawoo</h1>
+          <p className={styles.description}>
+            ⚠ CAUTION: This person is super cool!
+          </p>
+          <a href="#contact" className={styles.contactBtn}>
+            Contact Me
+          </a>
+        </div>
+        <img
+          src={getImageUrl("hero/heroImage.png")}
+          alt="rokawoo-mascot-hero"
+          className={styles.heroImg}
+          draggable="false"
+        />
+        <div className={styles.topBlur} />
+        <div className={styles.bottomBlur} />
+      </section>
     </div>
     </>
   );
--- rokawoo-react-website/src/components/Navbar/Navbar.tsx
+++ rokawoo-react-website/src/components/Navbar/Navbar.tsx
@@ -7,40 +7,42 @@ export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className={styles.navbar}>
-      <a className={styles.title} href="/">
-        Welcome to Rokaspace
-      </a>
-      <div className={styles.menu}>
-        <img
-          className={styles.menuBtn}
-          src={
-            menuOpen
-              ? getImageUrl("nav/closeIcon.png")
-              : getImageUrl("nav/menuIcon.png")
-          }
-          alt="menu-button"
-          draggable="false"
-          onClick={() => setMenuOpen(!menuOpen)}
-        />
-        <ul
-          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
-          onClick={() => setMenuOpen(false)}
-        >
-          <li className={styles.menuItem}>
-            <a href="#about">[About]</a>
-          </li>
-          <li className={styles.menuItem}>
-            <a href="#experience">[Experience]</a>
-          </li>
-          <li className={styles.menuItem}>
-            <a href="#projects">[Projects]</a>
-          </li>
-          <li className={styles.menuItem}>
-            <a href="#contact">[Contact]</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div className={styles.backgroundColor}>
+      <nav className={styles.navbar}>
+        <a className={styles.title} href="/">
+          Welcome to Rokaspace
+        </a>
+        <div className={styles.menu}>
+          <img
+            className={styles.menuBtn}
+            src={
+              menuOpen
+                ? getImageUrl("nav/closeIcon.png")
+                : getImageUrl("nav/menuIcon.png")
+            }
+            alt="menu-button"
+            draggable="false"
+            onClick={() => setMenuOpen(!menuOpen)}
+          />
+          <ul
+            className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+            onClick={() => setMenuOpen(false)}
+          >
+            <li className={styles.menuItem}>
+              <a href="#about">[About]</a>
+            </li>
+            <li className={styles.menuItem}>
+              <a href="#experience">[Experience]</a>
+            </li>
+            <li className={styles.menuItem}>
+              <a href="#projects">[Projects]</a>
+            </li>
+            <li className={styles.menuItem}>
+              <a href="#contact">[Contact]</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 };
