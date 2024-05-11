e223acc3c87e9462f349344538860d46bded10bd
Augustus Sroka <ays36@drexel.edu>
2024-05-11 15:45:19 -0400

    Added Comfortaa Font

--- rokawoo-react-website/src/main.tsx
+++ rokawoo-react-website/src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import "@fontsource/roboto"
+import "@fontsource/outfit"
+import '@fontsource-variable/comfortaa';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
