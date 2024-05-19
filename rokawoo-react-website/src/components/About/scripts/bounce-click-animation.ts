1af7d84d40a30077b2bdb98528deb4df76e97f8a
Roka <rokaawoo@gmail.com>
2024-05-19 17:19:18 -0400

    Optimised bounce-click-animation.ts

--- rokawoo-react-website/src/components/About/scripts/bounce-click-animation.ts
+++ rokawoo-react-website/src/components/About/scripts/bounce-click-animation.ts
@@ -1,8 +1,9 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useMemo, useEffect } from 'react';
 
 export const useBounceClickAnimation = (audioSrc: string) => {
   const [isAnimated, setIsAnimated] = useState<boolean>(false);
-  const [audio] = useState(new Audio(audioSrc));
+
+  const audio = useMemo(() => new Audio(audioSrc), [audioSrc]);
 
   const handleHover = useCallback(() => {
     setIsAnimated(true);
