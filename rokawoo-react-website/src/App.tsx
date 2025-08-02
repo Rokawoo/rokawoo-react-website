6e0a11a9a88efa80a99fde0d2325c1bf7f62b7d6
Rokawoo <rokaawoo@gmail.com>
2025-08-02 14:48:08 -0400

    Refactor Kins component into Showcase; update imports and styles

--- rokawoo-react-website/src/App.tsx
+++ rokawoo-react-website/src/App.tsx
@@ -5,7 +5,7 @@ import styles from "./App.module.css"
 import { Navbar } from "./components/Navbar/Navbar";
 import { Hero } from './components/Hero/Hero';
 import { About } from './components/About/About';
-import { Kins } from './components/Kins/Kins';
+import { Kins } from './components/Showcase/Showcase';
 import { Projects } from './components/Projects/Projects';
 import { Contact} from './components/Contact/Contact';
 
