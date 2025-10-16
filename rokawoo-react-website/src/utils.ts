8a1712f120ce53a6704338bae4f2c994645c145d
Rokawoo <rokaawoo@gmail.com>
2025-10-15 23:02:53 -0400

    Syntax and Style Fixes

--- rokawoo-react-website/src/utils.ts
+++ rokawoo-react-website/src/utils.ts
@@ -1,4 +1,4 @@
 export const getAssetUrl = (path: string): string => {
-    return new URL(`./assets/${path}`, import.meta.url).href;
-  };
+  return new URL(`./assets/${path}`, import.meta.url).href;
+};
   
