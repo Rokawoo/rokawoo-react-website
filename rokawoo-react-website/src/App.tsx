bbb323633136e18de3659ba525e995f190f56405
Augustus Sroka <rokaawoo@gmail.com>
2026-01-02 06:58:09 -0500

    Add smooth scrolling effect on hash link navigation

--- rokawoo-react-website/src/App.tsx
+++ rokawoo-react-website/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import styles from "./App.module.css"
 
@@ -7,10 +7,19 @@ import { Hero } from './components/Hero/Hero';
 import { About } from './components/About/About';
 import { Showcase } from './components/Showcase/Showcase';
 import { Projects } from './components/Projects/Projects';
-import { Contact} from './components/Contact/Contact';
+import { Contact } from './components/Contact/Contact';
 
 
 const App: React.FC = () => {
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      setTimeout(() => {
+        document.querySelector(hash)?.scrollIntoView({ behavior: 'smooth' });
+      }, 600);
+    }
+  }, []);
+
   return (
     <div className={styles.App}>
       <Navbar />
